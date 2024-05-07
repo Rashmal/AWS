7dfd714566bdffbf40e37a2027b0424d32509946
JayaminiAb <jayamini.amj@gmail.com>
2024-05-07 17:42:16 +0530

    boola boola UI

--- src/app/layout/module-selector/module-selector.component.ts
+++ src/app/layout/module-selector/module-selector.component.ts
@@ -7,46 +7,47 @@ import { CommonModel } from 'src/app/modules/common/models/commonModel';
 import { CommonService } from 'src/app/modules/common/services/common.service';
 
 @Component({
-  selector: 'app-module-selector',
-  templateUrl: './module-selector.component.html',
-  styleUrl: './module-selector.component.scss'
+    selector: 'app-module-selector',
+    templateUrl: './module-selector.component.html',
+    styleUrl: './module-selector.component.scss',
 })
 export class ModuleSelectorComponent implements OnInit {
-  numbers: number[] = [];
-  // Store the user role code
-  userRoleCode: string = '';
-  // Store the cookie interface
-  overallCookieInterface: OverallCookieInterface;
-  // Store common module
-  commonModel: CommonModel;
-  //Store module list
-  moduleList: Module[] = [];
-  constructor(private commonService: CommonService, private router: Router){
-    this.overallCookieInterface = new OverallCookieModel();
-    // Setting the user role
-    this.userRoleCode = this.overallCookieInterface
-        .GetUserRole()
-        .toUpperCase();
+    numbers: number[] = [];
+    // Store the user role code
+    userRoleCode: string = '';
+    // Store the cookie interface
+    overallCookieInterface: OverallCookieInterface;
+    // Store common module
+    commonModel: CommonModel;
+    //Store module list
+    moduleList: Module[] = [];
+    constructor(private commonService: CommonService, private router: Router) {
+        this.overallCookieInterface = new OverallCookieModel();
+        // Setting the user role
+        this.userRoleCode = this.overallCookieInterface
+            .GetUserRole()
+            .toUpperCase();
         this.commonModel = new CommonModel(this.commonService);
-  }
+    }
 
-  ngOnInit(): void {
-   this.getModuleAccessList();
-  }
+    ngOnInit(): void {
+        this.getModuleAccessList();
+    }
 
-   // Getting all the access list based on the user role for view
-   getModuleAccessList(){
-    this.commonModel.GetViewAccessListBasedUserRole(this.userRoleCode).then(
-      (data: Module[]) => {
-        this.moduleList = data;
-      }
-    );
+    // Getting all the access list based on the user role for view
+    getModuleAccessList() {
+        this.commonModel
+            .GetViewAccessListBasedUserRole(this.userRoleCode)
+            .then((data: Module[]) => {
+                this.moduleList = data;
+            });
+    }
 
-   }
-
-   clickOnModule(module: Module){
-     // Navigate to the layout
-     this.router.navigate(['/layout']);
-   }
-  
+    clickOnModule(module: Module) {
+      //check it is accessible
+        if (!module.IsDisable) {
+            // Navigate to the layout
+            this.router.navigate(['/layout']);
+        }
+    }
 }
